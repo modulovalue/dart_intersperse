d298966759eeab6eba389f8620a276a24c4b0dfb
Fré Dumazy <dumazy@gmail.com>
2020-12-25 23:54:57 +0100

    Add extension methods
    Extension methods can provide a cleaner and more readable way to use the library

--- lib/intersperse.dart
+++ lib/intersperse.dart
@@ -1,3 +1,4 @@
 library intersperse;
 
 export 'src/intersperse.dart';
+export 'src/intersperse_extensions.dart';
---
+++ test/extensions_test.dart
@@ -0,0 +1,51 @@
+import 'package:intersperse/intersperse.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group("intersperse extensions", () {
+    test("0", () {
+      expect(<int>[].intersperse(-1), <int>[]);
+    });
+    test("1", () {
+      expect(<int>[1].intersperse(-1), <int>[1]);
+    });
+    test("2", () {
+      expect(
+        <int>[1, 2].intersperse(-1),
+        <int>[1, -1, 2],
+      );
+    });
+    test("3", () {
+      expect(
+        <int>[1, 2, 3].intersperse(-1),
+        <int>[1, -1, 2, -1, 3],
+      );
+    });
+    test("is lazy", () {
+      final Iterable<int> sut =
+          List.generate(1000000, (a) => a).intersperse(-1);
+      expect(0, sut.first);
+    });
+  });
+
+  group("intersperseOuter extensions", () {
+    test("0", () {
+      expect(<int>[].intersperseOuter(-1), <int>[]);
+    });
+    test("1", () {
+      expect(<int>[1].intersperseOuter(-1), <int>[-1, 1, -1]);
+    });
+    test("2", () {
+      expect(<int>[1, 2].intersperseOuter(-1), <int>[-1, 1, -1, 2, -1]);
+    });
+    test("3", () {
+      expect(
+          <int>[1, 2, 3].intersperseOuter(-1), <int>[-1, 1, -1, 2, -1, 3, -1]);
+    });
+    test("is lazy", () {
+      final Iterable<int> sut =
+          List.generate(1000000, (a) => a).intersperseOuter(-1);
+      expect(-1, sut.first);
+    });
+  });
+}
